88c8ef6be5ed79c0016b0bd287099596b5682fba
stevenvenvenven <take.19940627@gmail.com>
2019-10-28 23:56:03 +0900

    ・add to main.dart： 　- タブ切り替え用のメソッドを用意

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Animecca',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Animecca'),
@@ -32,20 +23,13 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+
+PageController pageController;
 
 class _MyHomePageState extends State<MyHomePage> {
   int _page = 0;
@@ -70,6 +54,9 @@ class _MyHomePageState extends State<MyHomePage> {
             color: Colors.white,
           )
         ],
+        controller: pageController,
+        physics: NeverScrollableScrollPhysics(),
+        onPageChanged: onPageChanged,
       ),
       bottomNavigationBar: CupertinoTabBar(
         activeColor: Colors.orange,
@@ -99,7 +86,31 @@ class _MyHomePageState extends State<MyHomePage> {
             title: Container(height: 0.0),
             backgroundColor: Colors.white),
         ],
+        onTap: navigationTapped,
+        currentIndex: _page,
       )// This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanged (int page) {
+    setState(() {
+      this._page = page;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
   }
 }
